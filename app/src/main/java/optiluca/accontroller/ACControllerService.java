e626033b1b1c313e93d8eb86a5b244f271e490a1
Luca Bortolotti <optiluca@gmail.com>
2017-06-27 23:41:32 +0200

    Can start/stop AC via UDP packet!  To be tested, but seems ok (it beeps!)

--- app/src/main/java/optiluca/accontroller/ACControllerService.java
+++ app/src/main/java/optiluca/accontroller/ACControllerService.java
@@ -3,6 +3,7 @@ package optiluca.accontroller;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.hardware.ConsumerIrManager;
 import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.os.IBinder;
@@ -10,9 +11,17 @@ import android.util.Log;
 import android.app.Notification;
 import android.support.v4.app.NotificationCompat;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class ACControllerService extends Service {
 
     private boolean isRunning = false;
+
+    private static final int CONTROL_PORT = 11000;
+    private static final int STATUS_PORT = 11001;
 
     public ACControllerService() {
     }
@@ -48,12 +57,14 @@ public class ACControllerService extends Service {
             @Override
             public void run() {
 
+                ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM,100);
+
                 //Your logic that service will perform will be placed here
                 //In this example we are just looping and waits for 1000 milliseconds in each loop.
-                for (int i = 0; i < 5; i++) {
+                /*for (int i = 0; i < 5; i++) {
                     try {
                         Thread.sleep(1000);
-                        ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM,100);
+
                         toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD,200);
 
                     } catch (Exception e) {
@@ -62,7 +73,35 @@ public class ACControllerService extends Service {
                     if(isRunning){
                         Log.i("A", "Service running");
                     }
+                }*/
+
+                try {
+                    DatagramSocket dSocket = new DatagramSocket(CONTROL_PORT);
+                    byte[] buffer = new byte[1];
+                    DatagramPacket packet = new DatagramPacket(buffer,buffer.length);
+
+                    while(!dSocket.isClosed()) {
+                        dSocket.receive(packet);
+
+                        Log.i("PACKET", new String(packet.getData()));
+
+                        switch(packet.getData()[0]) {
+                            case 0x01:
+                                ACControllerBackend.ACOn((ConsumerIrManager) getSystemService(CONSUMER_IR_SERVICE),21);
+                                toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD,200);
+                                break;
+                            case 0x02:
+                                ACControllerBackend.ACOff((ConsumerIrManager) getSystemService(CONSUMER_IR_SERVICE));
+                                toneG.startTone(ToneGenerator.TONE_CDMA_ANSWER,200);
+                                break;
+                        }
+                        if (!isRunning) return;
+                    }
+                } catch(IOException e){
+                    e.printStackTrace();
                 }
+
+
             }
         }).start();
 
